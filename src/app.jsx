2487e8560b2cac9fc63f947cd8f1dbe594b0d5ed
Jorgerrs <jordixl90@gmail.com>
2025-06-28 14:58:57 +0200

    Add win/loss counter and monster abilities

--- src/app.jsx
+++ src/app.jsx
@@ -8,16 +8,126 @@ const rarities = {
 };
 
 const initialMonsters = [
-  { id: 1, name: "Bicho", rarity: "common", unlocked: true, image: "images/bicho.svg" },
-  { id: 2, name: "Topo", rarity: "common", unlocked: true, image: "images/topo.svg" },
-  { id: 3, name: "Rata", rarity: "common", unlocked: true, image: "images/rata.svg" },
-  { id: 4, name: "Lobo", rarity: "common", unlocked: false, image: "images/lobo.svg" },
-  { id: 5, name: "Orco", rarity: "poco comun", unlocked: false, image: "images/orco.svg" },
-  { id: 6, name: "Golem", rarity: "poco comun", unlocked: false, image: "images/golem.svg" },
-  { id: 7, name: "Harpia", rarity: "poco comun", unlocked: false, image: "images/harpia.svg" },
-  { id: 8, name: "Dragon", rarity: "epico", unlocked: false, image: "images/dragon.svg" },
-  { id: 9, name: "Kraken", rarity: "epico", unlocked: false, image: "images/kraken.svg" },
-  { id: 10, name: "Fenix", rarity: "legendario", unlocked: false, image: "images/fenix.svg" }
+  {
+    id: 1,
+    name: "Bicho",
+    rarity: "common",
+    unlocked: true,
+    image: "images/bicho.svg",
+    abilities: {
+      piedra: "Caparaz\u00f3n duro",
+      papel: "Envoltura pegajosa",
+      tijera: "Pinzas agudas"
+    }
+  },
+  {
+    id: 2,
+    name: "Topo",
+    rarity: "common",
+    unlocked: true,
+    image: "images/topo.svg",
+    abilities: {
+      piedra: "Golpe subterr\u00e1neo",
+      papel: "Mont\u00f3n de tierra",
+      tijera: "Ara\u00f1azo"
+    }
+  },
+  {
+    id: 3,
+    name: "Rata",
+    rarity: "common",
+    unlocked: true,
+    image: "images/rata.svg",
+    abilities: {
+      piedra: "Mordisco roedor",
+      papel: "Cola flexible",
+      tijera: "Ara\u00f1azo r\u00e1pido"
+    }
+  },
+  {
+    id: 4,
+    name: "Lobo",
+    rarity: "common",
+    unlocked: false,
+    image: "images/lobo.svg",
+    abilities: {
+      piedra: "Embestida",
+      papel: "Aullido aturdidor",
+      tijera: "Mordida salvaje"
+    }
+  },
+  {
+    id: 5,
+    name: "Orco",
+    rarity: "poco comun",
+    unlocked: false,
+    image: "images/orco.svg",
+    abilities: {
+      piedra: "Mazo pesado",
+      papel: "Escudo improvisado",
+      tijera: "Hachazo"
+    }
+  },
+  {
+    id: 6,
+    name: "Golem",
+    rarity: "poco comun",
+    unlocked: false,
+    image: "images/golem.svg",
+    abilities: {
+      piedra: "Pu\u00f1o de roca",
+      papel: "Torbellino de polvo",
+      tijera: "Aplastamiento"
+    }
+  },
+  {
+    id: 7,
+    name: "Harpia",
+    rarity: "poco comun",
+    unlocked: false,
+    image: "images/harpia.svg",
+    abilities: {
+      piedra: "Picotazo",
+      papel: "Plumas cortantes",
+      tijera: "Garras r\u00e1pidas"
+    }
+  },
+  {
+    id: 8,
+    name: "Dragon",
+    rarity: "epico",
+    unlocked: false,
+    image: "images/dragon.svg",
+    abilities: {
+      piedra: "Golpe de cola",
+      papel: "Ala protectora",
+      tijera: "Aliento de fuego"
+    }
+  },
+  {
+    id: 9,
+    name: "Kraken",
+    rarity: "epico",
+    unlocked: false,
+    image: "images/kraken.svg",
+    abilities: {
+      piedra: "Tent\u00e1culo poderoso",
+      papel: "Tinta envolvente",
+      tijera: "Agarre cortante"
+    }
+  },
+  {
+    id: 10,
+    name: "Fenix",
+    rarity: "legendario",
+    unlocked: false,
+    image: "images/fenix.svg",
+    abilities: {
+      piedra: "Llama eterna",
+      papel: "Canto revitalizador",
+      tijera: "Vuelo abrasador"
+    }
+  }
 ];
 
 const choices = ["piedra", "papel", "tijera"];
@@ -43,6 +153,8 @@ function App() {
   const [monsters, setMonsters] = useState(initialMonsters);
   const [playerMonsterId, setPlayerMonsterId] = useState(1);
   const [battleCount, setBattleCount] = useState(0);
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
   const [result, setResult] = useState(null);
   const [lastBattle, setLastBattle] = useState(null);
 
@@ -55,9 +167,15 @@ function App() {
     const winner = getWinner(move, playerMonster, enemyMove, enemyMonster);
 
     let message;
-    if (winner === "player") message = "\u00A1Ganaste!";
-    else if (winner === "enemy") message = "Perdiste";
-    else message = "Empate";
+    if (winner === "player") {
+      message = "\u00A1Ganaste!";
+      setWins(wins + 1);
+    } else if (winner === "enemy") {
+      message = "Perdiste";
+      setLosses(losses + 1);
+    } else {
+      message = "Empate";
+    }
     setResult(message);
     setLastBattle({
       playerMonster,
@@ -82,7 +200,9 @@ function App() {
   return (
     <div>
       <h1>Combate de Monstruos</h1>
-      <p>Combates: {battleCount}</p>
+      <p>
+        Combates: {battleCount} | Victorias: {wins} | Derrotas: {losses}
+      </p>
 
       <div>
         <h2>Selecciona tu monstruo</h2>
@@ -125,7 +245,9 @@ function App() {
             width="60"
             style={{ display: "block", marginBottom: "10px" }}
           />
-          <p>Tu movimiento: {lastBattle.playerMove}</p>
+          <p>
+            Tu movimiento: {lastBattle.playerMonster.abilities[lastBattle.playerMove]} ({lastBattle.playerMove})
+          </p>
           <h3>
             Enemigo: {lastBattle.enemyMonster.name} ({lastBattle.enemyMonster.rarity})
           </h3>
@@ -135,7 +257,9 @@ function App() {
             width="60"
             style={{ display: "block", marginBottom: "10px" }}
           />
-          <p>Movimiento del enemigo: {lastBattle.enemyMove}</p>
+          <p>
+            Movimiento del enemigo: {lastBattle.enemyMonster.abilities[lastBattle.enemyMove]} ({lastBattle.enemyMove})
+          </p>
         </div>
       )}
 
